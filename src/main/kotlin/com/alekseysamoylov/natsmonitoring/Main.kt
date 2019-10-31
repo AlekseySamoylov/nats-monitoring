ca34239201b787ac104197d072b161d16cef3f17
Aleksey Samoylov <alekseysamoylov89@gmail.com>
2019-10-31 06:16:18 +0400

    polishing

--- src/main/kotlin/com/alekseysamoylov/natsmonitoring/Main.kt
+++ src/main/kotlin/com/alekseysamoylov/natsmonitoring/Main.kt
@@ -1,6 +1,7 @@
 package com.alekseysamoylov.natsmonitoring
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.kittinunf.fuel.core.FuelError
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
 import kotlinx.serialization.Serializable
@@ -16,65 +17,75 @@ import org.elasticsearch.client.indices.GetIndexRequest
 import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.common.xcontent.XContentType
 import java.util.*
+import kotlin.concurrent.schedule
 
 
 class Main {
 }
 
-fun main() {
-    val (request, response, result) = "http://localhost:8222/streaming/channelsz?subs=1"
-        .httpGet()
-        .responseString()
-    val json = Json(JsonConfiguration(
-        encodeDefaults = true,
-        strictMode = false,
-        unquoted = false,
-        allowStructuredMapKeys = true,
-        prettyPrint = false,
-        indent = "    ",
-        useArrayPolymorphism = false,
-        classDiscriminator = "type"
-    ))
+lateinit var restHighLevelClient: RestHighLevelClient
 
-    when (result) {
+fun main() {
+    restHighLevelClient = RestHighLevelClient(
+        RestClient.builder(
+            HttpHost("localhost", 9200, "http"),
+            HttpHost("localhost", 9201, "http")
+        )
+    )
+
+    println("Press ENTER to exit")
+    try {
+        Timer().schedule(0L, 5000L) { uploadNatsMetricsToElastic() }
+        readLine()
+    } finally {
+        restHighLevelClient.close()
+    }
+}
+
+fun uploadNatsMetricsToElastic() {
+    when (val result = requestNatsStats()) {
         is Result.Failure -> {
             val ex = result.getException()
             println(ex)
         }
         is Result.Success -> {
-            val data = result.get()
-            println(data)
-            val dataObj = json.parse(Data.serializer(), data)
-            println(dataObj)
+            proceedSuccessfulNatsMetricsResponse(result)
         }
     }
-
-
-
 }
 
+fun proceedSuccessfulNatsMetricsResponse(result: Result<String, FuelError>) {
+    val data = result.get()
+    println(data)
+    val dataObj = json.parse(Data.serializer(), data)
+    for (channel in dataObj.channels) {
+        createElasticIndexIfAbsentAndAddDataToElastic(channel)
+    }
+    println(dataObj)
+}
+
+fun requestNatsStats(): Result<String, FuelError> {
+    val (request, response, result) = "http://localhost:8222/streaming/channelsz?subs=1"
+        .httpGet()
+        .responseString()
+    return result
+}
 
 fun createElasticIndexIfAbsentAndAddDataToElastic(channel: Channel) {
-    val restHighLevelClient = RestHighLevelClient(
-        RestClient.builder(
-            HttpHost("localhost", 9200, "http"),
-            HttpHost("localhost", 9201, "http")))
-
-    val dataMessage = HashMap<String, Any>()
 
 
-    val channelMessage = HashMap<String, Any>()
-    channelMessage["type"] = "text"
-    val properties = HashMap<String, Any>()
-    properties["userId"] = channelMessage
-    properties["name"] = channelMessage
+    val textMessage = HashMap<String, Any>()
+    textMessage["type"] = "text"
+    val integerMessage = HashMap<String, Any>()
+    integerMessage["type"] = "integer"
+    val channelProperties = HashMap<String, Any>()
+    channelProperties["name"] = textMessage
+    channelProperties["msgs"] = integerMessage
     val mapping = HashMap<String, Any>()
-    mapping["properties"] = properties
+    mapping["properties"] = channelProperties
 
     val createIndexRequest = CreateIndexRequest(CH_INDX)
-    createIndexRequest.settings( Settings.builder()
-        .put("index.number_of_shards", 1)
-        .put("index.number_of_replicas", 2))
+    createIndexRequest.settings(Settings.EMPTY)
 
     createIndexRequest.mapping(mapping)
 
@@ -89,15 +100,26 @@ fun createElasticIndexIfAbsentAndAddDataToElastic(channel: Channel) {
     request3.id(UUID.randomUUID().toString())
     request3.source(ObjectMapper().writeValueAsString(channel), XContentType.JSON)
     val indexResponse = restHighLevelClient.index(request3, RequestOptions.DEFAULT)
-    println("response id: " + indexResponse.getId())
-    println("response id: " + indexResponse.getResult().name)
-
-
-    restHighLevelClient.close()
+    println("response id: " + indexResponse.id)
+    println("response id: " + indexResponse.result.name)
 }
 
 @Serializable
 data class Channel(val name: String, val msgs: Int)
+
 @Serializable
 data class Data(val offset: Int, val channels: List<Channel>)
+
 private const val CH_INDX = "channel_index"
+private val json = Json(
+    JsonConfiguration(
+        encodeDefaults = true,
+        strictMode = false,
+        unquoted = false,
+        allowStructuredMapKeys = true,
+        prettyPrint = false,
+        indent = "    ",
+        useArrayPolymorphism = false,
+        classDiscriminator = "type"
+    )
+)
